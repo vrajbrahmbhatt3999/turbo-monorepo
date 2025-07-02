42be618b30e59621d0ccd4814c30316f43b75706
Vraj Brahmbhatt <vraj.brahmbhatt@ethicsinfotech.in>
2025-07-02 18:51:50 +0530

    Implemented casl for permissions

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { AbilityContext, defineAbilitiesFromPermissions } from '@my-monorepo/core';
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+  
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+async function init() {
+  const roleDoc = {
+  _id: 'role_cashier',
+  type: 'role_permission',
+  role: 'cashier',
+  modules: [
+    {
+      name: 'sales_invoice',
+      permissions: ['create', 'read', 'view']
+    },
+    {
+      name: 'returns',
+      permissions: ['read']
+    }
+  ]
+};
+  const ability = defineAbilitiesFromPermissions(roleDoc);
+ 
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <AbilityContext.Provider value={ability}>
+        <App />
+      </AbilityContext.Provider>
+    </React.StrictMode>
+  );
+}
+ 
+init();
